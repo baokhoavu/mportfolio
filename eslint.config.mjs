53561c33dfccc67843e899f8376dd441406cf475
baokhoavu <baokhoadinhvu@gmail.com>
2025-12-14 20:42:35 -0800

    style(format): apply CI-specific formatting fixes (eslint import order, Mailchimp)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
 import { defineConfig, globalIgnores } from "eslint/config";
+import nextTs from "eslint-config-next/typescript";
 
 // Recommended flat config for Next.js projects using the
 // Next-provided shareable configs. This avoids using
