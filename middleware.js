3745aa602fbd4169c195598df1e3b5a8bdb0b9d8
baokhoavu <baokhoadinhvu@gmail.com>
2025-12-23 09:51:14 -0800

    Fix caching:troubleshoot;

---
+++ middleware.js
@@ -0,0 +1,10 @@
+// This file sets custom HTTP headers for all routes in the Next.js app.
+export default function middleware(req) {
+  const res = NextResponse.next();
+  res.headers.set('Cache-Control', 'no-store, must-revalidate');
+  return res;
+}
+
+export const config = {
+  matcher: '/:path*',
+};
